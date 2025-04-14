a7aad5dd38c54c00b70690120c92608e317077a4
kavinduhapu <hapurox@gmail.com>
2025-04-14 10:30:21 +0530

    Test math and weather server

---
+++ multiple_client.py
@@ -0,0 +1,35 @@
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+
+import asyncio
+import dotenv
+
+dotenv.load_dotenv()
+from langchain_openai import ChatOpenAI
+model = ChatOpenAI(model="gpt-4o")
+
+async def main():
+    async with MultiServerMCPClient(
+        {
+            "math": {
+                "command": "python",
+                # Make sure to update to the full absolute path to your math_server.py file
+                "args": ["F:\Data Science\lang-chain\mcp-test\math_server.py"],
+                "transport": "stdio",
+            },
+            "weather": {
+                # make sure you start your weather server on port 8000
+                "url": "http://localhost:8000/sse",
+                "transport": "sse",
+            }
+        }
+    ) as client:
+        agent = create_react_agent(model, client.get_tools())
+        math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
+        weather_response = await agent.ainvoke({"messages": "what is the weather in nyc?"})
+        print(math_response)
+        print(weather_response)
+
+
+# Run the async function
+asyncio.run(main())
