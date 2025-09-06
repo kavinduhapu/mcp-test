672cc4725e98c7a08418abc401bc6ff083c26896
kavinduhapu <hapurox@gmail.com>
2025-09-06 23:12:00 +0530

    Update client to use new mcp package

--- multiple_client.py
+++ multiple_client.py
@@ -5,30 +5,35 @@ import asyncio
 import dotenv
 
 dotenv.load_dotenv()
-from langchain_openai import ChatOpenAI
-model = ChatOpenAI(model="gpt-4o")
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+# model = ChatOpenAI(model="gpt-4o")
+model = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
 
 async def main():
-    async with MultiServerMCPClient(
+
+    client = MultiServerMCPClient(
         {
             "math": {
                 "command": "python",
                 # Make sure to update to the full absolute path to your math_server.py file
-                "args": ["F:\Data Science\lang-chain\mcp-test\math_server.py"],
+                "args": ["/home/kavindu-hapuarachchi/data-science-projects/mcp-test/math_server.py"],
                 "transport": "stdio",
             },
             "weather": {
-                # make sure you start your weather server on port 8000
+                # Make sure you start your weather server on port 8000
                 "url": "http://localhost:8000/sse",
                 "transport": "sse",
             }
         }
-    ) as client:
-        agent = create_react_agent(model, client.get_tools())
-        math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
-        weather_response = await agent.ainvoke({"messages": "what is the weather in nyc?"})
-        print(math_response)
-        print(weather_response)
+    )
+    tools = await client.get_tools()
+    agent = create_react_agent(model, tools)
+    math_response = await agent.ainvoke({"messages": "what's (3 + 5) x 12?"})
+    weather_response = await agent.ainvoke({"messages": "what is the weather in nyc?"})
+
+    print(math_response)
+    print(weather_response)
 
 
 # Run the async function
